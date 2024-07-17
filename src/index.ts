b3a219c338250ff3d359a0d32740865e01434705
Oriianamendez <egleemendez@gmail.com>
2024-07-17 10:36:03 +0200

    create db array
    Co-authored-by: Lukas Låås <lukas-laas@users.noreply.github.com>
    Co-authored-by: Harald Norlin <Hano2001@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,28 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const db = [
+  {
+    cartid: "82479462-af95-4b88-bcef-4bf8f4261a4a",
+    cartItems: [
+      {
+        productId: "3a9f1a05-390e-4109-8072-ac7a1caa7001",
+        name: "A key ring",
+        price: 0.85,
+        quantity: 2,
+      },
+      {
+        productId: "fe3e8d33-a0bc-4606-b1b3-eb59b645b94b",
+        name: "Playing cards",
+        price: 4.85,
+        quantity: 4,
+      },
+    ],
+    totalNumberOfItems: 6,
+    totalPrice: 21.1,
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
