f40ea35969f464706cf7f2d554d2bc75e42b0c0c
Oriianamendez <egleemendez@gmail.com>
2024-07-19 11:26:01 +0200

    remove mock db
    Co-authored-by: Lukas Låås <lukas-laas@users.noreply.github.com>
    Co-authored-by: Harald Norlin <Hano2001@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -5,29 +5,6 @@ import { eq } from "drizzle-orm";
 
 const app = express();
 const port = 3000;
-
-const database = [
-  {
-    cartid: "0",
-    cartItems: [
-      {
-        productId: "3a9f1a05-390e-4109-8072-ac7a1caa7001",
-        name: "A key ring",
-        price: 0.85,
-        quantity: 2,
-      },
-      {
-        productId: "fe3e8d33-a0bc-4606-b1b3-eb59b645b94b",
-        name: "Playing cards",
-        price: 4.85,
-        quantity: 4,
-      },
-    ],
-    totalNumberOfItems: 6,
-    totalPrice: 21.1,
-  },
-];
-
 app.use(json());
 
 app.get("/", (req, res) => {
@@ -71,17 +48,9 @@ app.post("/api/products", async (req, res) => {
 
   res.json(result);
 });
-app.post("/api/carts/:cartId/products", (req, res) => {
-  const { name, description, price } = req.body;
-  const cart = database.find((c) => (c.cartid = req.params.cartId));
-  cart?.cartItems.push({
-    productId: productId,
-    name: "Banana",
-    description: "Yellow and bent",
-    price: 1.32,
-  });
 
-  res.json(cart);
+app.post("/api/carts/:cartId/products", async (req, res) => {
+  console.log(res, req);
 });
 
 app.delete("/api/carts/:cartId", async (req, res) => {
