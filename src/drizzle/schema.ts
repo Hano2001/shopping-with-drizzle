935cc368c2a7aaa76ff5628c6d7a59058cdd2a03
Oriianamendez <egleemendez@gmail.com>
2024-07-19 10:14:43 +0200

    add not null in required fields
    Co-authored-by: Lukas Låås <lukas-laas@users.noreply.github.com>
    Co-authored-by: Harald Norlin <Hano2001@users.noreply.github.com>

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,24 +1,17 @@
 import { relations } from "drizzle-orm";
-import {
-  pgTable,
-  text,
-  integer,
-  uuid,
-  decimal,
-  primaryKey,
-} from "drizzle-orm/pg-core";
+import { pgTable, text, integer, uuid, decimal } from "drizzle-orm/pg-core";
 
 export const products = pgTable("products", {
-  productId: uuid("productId").defaultRandom().primaryKey(),
-  name: text("name"),
+  productId: uuid("productId").defaultRandom().primaryKey().notNull(),
+  name: text("name").notNull(),
   description: text("description"),
-  price: decimal("price"),
+  price: decimal("price").notNull(),
 });
 
 export const carts = pgTable("carts", {
-  cartId: uuid("cartId").defaultRandom().primaryKey(),
-  totalNumberOfItems: integer("totalNumberOfItems"),
-  totalPrice: decimal("totalPrice"),
+  cartId: uuid("cartId").defaultRandom().primaryKey().notNull(),
+  totalNumberOfItems: integer("totalNumberOfItems").notNull(),
+  totalPrice: decimal("totalPrice").notNull(),
 });
 
 export const cartProducts = pgTable(
@@ -26,14 +19,10 @@ export const cartProducts = pgTable(
   {
     cartId: uuid("cartId").references(() => carts.cartId),
     productId: uuid("productId").references(() => products.productId),
-    quantity: integer("quantity"),
+    quantity: integer("quantity").notNull(),
   },
   (table) => ({
     primaryKey: [table.cartId, table.productId],
-    // cartProductKey: primaryKey({
-    //   name: "cart_product_key",
-    //   columns: [table.cartId, table.productId],
-    // }),
   })
 );
 
